10eb6a30739c9527247a7c4484de2a43ea0443c4
kumarpit <kumar.arpit77@yahoo.com>
2022-09-09 14:48:54 -0700

    memoization

--- dynamic-programming/can_sum.cpp
+++ dynamic-programming/can_sum.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<unordered_map>
 
 
 /**
@@ -8,21 +9,25 @@ Write a function that takes in a targetSum and an array of integers and returns 
 
 using namespace std;
 
-// O(n^m)
-bool can_sum(int target, vector<int> nums) {
+// O()
+bool can_sum(int target, vector<int> nums, unordered_map<int, bool> &memo) {
+    if (memo.count(target) > 0) return memo[target];
     if (target < 0) return false;
     if (target == 0) return true;
 
     for (int i=0; i < nums.size(); i++) {
         int remainder = target - nums[i];
-        int possible = can_sum(remainder, nums);
+        int possible = can_sum(remainder, nums, memo);
         if (possible) return true;
+        else memo[remainder] = false;
     }
 
+    memo[target] = false;
     return false;
 }
 
 int main() {
-    vector<int> nums = {8, 5};
-    cout << can_sum(9, nums) << endl;
+    vector<int> nums = {7, 14};
+    unordered_map<int, bool> memo;
+    cout << can_sum(300, nums, memo) << endl;
 }
